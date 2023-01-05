434286fe78a1965cda30c1e26900781f9050c972
Kyle Jenne <strongagent1@gmail.com>
2023-01-05 10:56:40 -0500

    CarLot simulator classes exercise - KJ

---
+++ CarLotSimulator/CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+    
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+        
+
+        public void MakeEngineNoise (string model, string engineNoise)
+        {
+            Console.WriteLine($" The {model} makes an engine sound like {engineNoise}!");
+        }
+        public void MakeHonkNoise(string model, string honkNoise)
+        {
+            Console.WriteLine($"The {model} makes an horn sound like {honkNoise}!");
+        }
+        
+    }
+}
---
+++ CarLotSimulator/CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -0,0 +1,31 @@
+﻿using CarLotSimulator;
+using System;
+//TODO
+
+//Create a seperate class file called Car-DONE
+//Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable-DONE
+//Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()-DONE
+//The methods should take one string parameter: the respective noise property-DONE
+CarLot myLot = new CarLot();
+
+
+//Now that the Car class is created we can instanciate 3 new cars-DONE
+//Set the properties for each of the cars-DONE
+//Call each of the methods for each car-DONE
+var firstCar = new Car() { Year = 2021, Make = "GMC", Model = "YukonXL", EngineNoise = "grrrmble grrrmble", HonkNoise = "Wahhhnnn", IsDriveable = true };
+var secondCar = new Car() { Year = 2004, Make = "Ford", Model = "Taurus", EngineNoise = "tiktok", HonkNoise = "beep beep", IsDriveable = false };
+var thirdCar = new Car() { Year = 2020, Make = "Hyundai", Model = "SantaFe", EngineNoise = "hmmmmmm", HonkNoise = "bip bip", IsDriveable = true };
+//*************BONUS*************//-DONE
+myLot.CarList.Add(firstCar);
+myLot.CarList.Add(secondCar);
+myLot.CarList.Add(thirdCar);
+// Set the properties utilizing the 3 different ways we learned about, one way for each car-DONE
+Console.WriteLine($"Our inventory currently is composed of....");
+//*************BONUS X 2*************//-DONE
+foreach (var car in myLot.CarList)
+{
+    Console.WriteLine($"{car.Year} {car.Make} {car.Model}.");
+
+
+
+}
